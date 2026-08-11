3ae1569a489e7cc873ceebe8772c6056cebd0126
vivekkk-007 <kumarvivek05093896@gmail.com>
2026-08-11 21:20:37 +0530

    bronze

---
+++ dags/flight-pipeline.py
@@ -0,0 +1,31 @@
+import sys
+from pathlib import Path
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+AIRFLOW_HOME = Path("/opt/airflow")
+
+if str(AIRFLOW_HOME) not in sys.path:
+    sys.path.insert(0, str(AIRFLOW_HOME))
+
+from scripts.bronze_layer import run_bronze_ingestion
+
+default_args = {
+    "owner": "airflow",
+    "retries": 0,
+    "retry_delay" : timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="flights_ops_medallion_pipe",
+    default_args=default_args,
+    start_date=datetime(2026, 8, 11),
+    schedule_interval="*/30 * * * *",
+    catchup=False,
+) as dag:
+
+    bronze = PythonOperator(
+        task_id="bronze_ingest",
+        python_callable=run_bronze_ingestion,
+    )
